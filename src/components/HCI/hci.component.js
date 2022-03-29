7a5b85bd1bfeeeaf13a1346636dffea98344c69c
Cheng Loo <60029991+interritus141@users.noreply.github.com>
2022-03-29 16:55:17 +0100

    fixed spelling and grammar for HCI page

--- src/components/HCI/hci.component.js
+++ src/components/HCI/hci.component.js
@@ -13,24 +13,24 @@ const HCI = () => {
           <div className="case">Usability</div>
           <p>
             A complex dashboard with overabundance of information is the last
-            thing staffs need in a hurry. To avoid this, we will focus on
-            learnability, efficiency, Memorability, and Satisfaction.
+            thing staff need in a hurry. To avoid this, we will focus on
+            learnability, efficiency, memorability, and satisfaction.
             <br />
             <br />
             Our dashboard design theme will be based on{" "}
             <a href="https://material.io/">Material Design</a>, using edges,
             shadows, and paper-like UI component to mimic real-world objects.
             Using material design principles, we aim to increase user
-            satisfaction by demonstrating a high quality and eye pleasing user
+            satisfaction by demonstrating a high quality and eye-pleasing user
             interface. It would also make things more familiar for users as
-            material ui is also used by chrome, android, and many other
+            Material UI is also used by Chrome, Android, and many other
             applications.
             <br />
             <br />
             For our dashboard to be learnable and efficient, the data needs to
             be organized and customizable. Filtering options should allow
             finding singular and sets of data. Data should be displayed in an
-            organized and common format like data tables with the options to
+            organized and common format like data tables with the option to
             reduce columns. Data should be able to be displayed under all tablet
             and computer resolutions for further accessibility. We also
             accommodate mobile devices; However, we won't recommend it because
@@ -40,7 +40,7 @@ const HCI = () => {
             Lastly, we aim to reduce interaction cost by making all the features
             easily navigatable. Each interaction must carry the user closer to
             their goals and user should find their desired data in reasonable
-            clicks. The most memorable designs are often simplest to navigate.
+            clicks. The most memorable designs are often the simplest to navigate.
           </p>
         </div>
       </div>
@@ -67,8 +67,8 @@ const HCI = () => {
           <p>
             Sketch 1 displayed data in a large organized table. With more
             complex data in development, the column will need to become
-            collapsable. The search function is a place holder for more advanced
-            and intuitive filtering in actual developoment.
+            collapsable. The search function is a placeholder for more advanced
+            and intuitive filtering in actual development.
           </p>
           <div className="case">Sketch 2</div>
           <img
@@ -89,12 +89,12 @@ const HCI = () => {
           </p>
           <div className="case">Comparison</div>
           <p>
-            Sketch 1 is more intuitive and simple. Data are clearly presented
-            and requries less interaction from the user. Sketch 2 takes a more
+            Sketch 1 is more intuitive and simple. Data is clearly presented
+            and requires fewer interactions from the user. Sketch 2 takes a more
             complex approach to UI which the user may find useful. Based on the
-            UI requirement, sketch 1 was our choice. It provided learnability,
+            UI requirements, sketch 1 was our choice. It provided learnability,
             clearity, and memorability which exceeds the sketch 2's possible
-            benifits. However, sketch 1 is also not perfect and we will be
+            benefits. However, sketch 1 is also not perfect and we will be
             enhancing them as we enter prototyping and development stages.
           </p>
         </div>
@@ -105,30 +105,30 @@ const HCI = () => {
           <div className="case">Wireframe 1</div>
           <p>
             Based on the sketch, we made the first version of our interactive
-            wireframes on figma. We followed most of the layouts on sketch 1 but
+            wireframes on Figma. We followed most of the layouts on sketch 1 but
             made some minor adjustments. The following is the picture of the
-            login and data display. For reason disscussed below, this was not
+            login and data display. For reasons discussed below, this was not
             our final wireframe.
           </p>
           <img className="smallimg" src="images/wf1_2.png" alt="" />
           <img className="smallimg" src="images/wf1_1.png" alt="" />
           <p>
-            There wasn't any significant problem with this design; However, we
+            There wasn't any significant problems with this design; However, we
             felt it was not good enough. Firstly, it would not be very
-            extendible with new features if another group were to carry on with
-            this project. As a POC, we want our app to be a good foundation for
+            extendable with new features if another group were to carry on with
+            this project. As a proof of concept (PoC), we want our app to be a good foundation for
             future work. Secondly, the UI design does not demonstrate the
-            material UI theme we planned for the actual UI frontend. During
+            Material UI theme we planned for the actual UI frontend. During
             simulated user testing, we noted that the design is not
-            particularily eye pleasing. Lastly, we wanted to include at least
-            some analytics in the "could have" sections of the MoSCoW list, and
+            particularly eye pleasing. Lastly, we wanted to include at least
+            some analytics in the "Could Have" sections of the MoSCoW list, and
             this, as a template to work upon, does not seem ideal.
           </p>
           <div className="case">Wireframe 2</div>
           <p>
             Learning the lesson from Wireframe 1, we switched up the tools to
-            adobe XD and designed with many additional features in mind. The
-            following is created using the adobe material UI pack.
+            Adobe XD and designed with many additional features in mind. The
+            following is created using the Adobe Material UI pack.
           </p>
           <img className="smallimg" src="images/wf2_1.png" alt="" />
           <img className="smallimg" src="images/wf2_2.png" alt="" />
@@ -136,8 +136,8 @@ const HCI = () => {
             Although the basic functionalities are the same, we thought that
             this version was a better template to begin development with. It had
             a more intuitive and navigatable layout. Tables and controls seem to
-            be much more organized. With the card based layout, It also has a
-            lot more room we can extend features into.
+            be much more organized. With a card-based layout, it also has a
+            lot more room for which we can extend features.
           </p>
         </div>
       </div>
@@ -146,9 +146,9 @@ const HCI = () => {
         <div className="subtitle">Highlights</div>
         <div className="text">
           <p>
-            With the decision in HCI, we created a solid foundation to build up
-            a our application. Following are pictures from the home page which
-            we think demonstrates the manifestation perfectly.
+            With the decisions in HCI, we created a solid foundation to build up
+            our application. The following are pictures from the home page which
+            we think demonstrates our vision perfectly.
           </p>
           <div className="case">Material Design</div>
           <img className="smallimg" src="images/fash.png" alt="" />
@@ -168,18 +168,18 @@ const HCI = () => {
         <div className="text">
           <div className="case">NHS Server Integration Dashboard</div>
           <p>
-            The mock application for api syncing interaction/admin dashboard was
+            The mock application for API syncing interaction/admin dashboard was
             not designed specifically for user usage since it was created due to
             limitations of integration with any existing NHS servers. It
             followed the single rule of simplicity while allowing it to simulate
-            the operations the server would take to make the same syncing api
+            the operations the server would take to make the same syncing API
             calls.
           </p>
           <div className="case">Scanning App</div>
           <p>
-            The scanning app was created due to limitation in manufacturing pcb
-            and custom devices in this project. It was never intened to have a
-            user interface during deployment and the poc we created for it only
+            The scanning app was created due to limitations in manufacturing printed circuit boards (PCBs)
+            and custom devices in this project. It was never intended to have a
+            user interface during deployment and the PoC we created for it only
             needed to scan and make a request; Therefore, it would not make
             sense for it to be included in HCI.
           </p>
