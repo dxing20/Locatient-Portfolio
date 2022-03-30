84073f92a0aad900ceb42ba17fe55e327cae67d4
Cheng Loo <60029991+interritus141@users.noreply.github.com>
2022-03-30 15:37:18 +0100

    added appendix links

--- src/components/appendix/appendix.component.js
+++ src/components/appendix/appendix.component.js
@@ -12,7 +12,34 @@ const Appendix = () => {
         <div className="text">
           <a href="files/UserManual.pdf">User Manual PDF</a>
         </div>
+
+        <div className="subtitle">User Manual</div>
+        <div className="text">
+          <a href="files/GDPR and Privacy of Data.pdf">GDPR and Privacy of Data PDF</a>
+        </div>
+
+        <div className="subtitle">EULA</div>
+        <div className="text">
+          <a href="files/EULA-agreement.pdf">EULA PDF</a>
+        </div>
+
+        <div className="subtitle">Licensing</div>
+        <div className="text">
+          <div className="case">Licensing Table</div>
+          <img
+            src="images/licensing.png"
+            alt=""
+          />
+          <br />
+          <br />
+          <div className="subtitle">Licensing Chart</div>
+          <img
+            src="images/licensing-chart.png"
+            alt=""
+          />
+        </div>
       </div>
+      <div className="spacing"></div>
     </div>
   );
 };
